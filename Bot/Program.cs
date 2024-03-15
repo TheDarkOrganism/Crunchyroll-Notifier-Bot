f8ed7bfe5fd6b4def9bf1de2ca2b96c3c420d186
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2024-03-14 18:12:12 -0700

    Added Config
    Added Config.json for setting the token and bot config.

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bot;
+using DSharpPlus;
+using System.Text.Json;
+
+const string configPath = "./Config.json";
+
+Config config;
+
+try
+{
+	Config? c = await JsonSerializer.DeserializeAsync<Config>(File.OpenRead(configPath));
+
+	if (c is null || string.IsNullOrWhiteSpace(c.Token))
+	{
+		Console.WriteLine("Missing Token");
+
+		return;
+	}
+
+	config = c;
+}
+catch
+{
+	Console.WriteLine($"Unable to read token from \"{configPath}\"");
+
+	return;
+}
+
+using DiscordClient discord = new(new()
+{
+	Token = config.Token,
+	TokenType = TokenType.Bot,
+	Intents = DiscordIntents.MessageContents
+});
+
+await discord.ConnectAsync();
+
+Console.WriteLine("Bot Started");
