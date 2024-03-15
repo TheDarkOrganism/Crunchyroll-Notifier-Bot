926925255986ce55daf770b2457f34f1f1dcfdc2
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2024-03-14 20:31:14 -0700

    Added Attributes
    Added TextChannelAttribute.cs

---
+++ Bot/Attributes/TextChannelAttribute.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Bot.Attributes
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class TextChannelAttribute : CheckBaseAttribute
+	{
+		public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+		{
+			return Task.FromResult(ctx.Channel.Type == DSharpPlus.ChannelType.Text);
+		}
+	}
+}
