2ef37a7183fb8c6fb91b42fd12de38956a945a20
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2024-03-15 20:20:22 -0700

    Added Main Services
    Added dependency injection and main loop.

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Bot;
-using DSharpPlus;
+using Bot.Sources;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.SlashCommands;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
 
 const string configPath = "./Config.json";
@@ -15,6 +17,8 @@ try
 	{
 		Console.WriteLine("Missing Token");
 
+		_ = Console.ReadKey();
+
 		return;
 	}
 
@@ -23,6 +27,8 @@ try
 catch
 {
 	Console.WriteLine($"Unable to read token from \"{configPath}\"");
+
+	_ = Console.ReadKey();
 
 	return;
 }
@@ -34,12 +40,68 @@ using DiscordClient discord = new(new()
 	MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug
 });
 
-SlashCommandsExtension slash = discord.UseSlashCommands();
+ServiceCollection services = new();
+
+services.AddSingleton(discord);
+services.AddSingleton<Channels>();
+services.AddSingleton<Data>();
+services.AddSingleton<RSSSource>();
+
+ServiceProvider provider = services.BuildServiceProvider();
+
+discord.UseCommandsNext(new()
+{
+	Services = provider
+});
+
+SlashCommandsExtension slash = discord.UseSlashCommands(new()
+{
+	Services = provider
+});
 
 slash.RegisterCommands<SlashCommands>();
+
+discord.Ready += async (client, readArgs) =>
+{
+	RSSSource? rss = provider.GetService<RSSSource>();
+
+	if (rss is null)
+	{
+		return;
+	}
+
+	using PeriodicTimer timer = new(TimeSpan.FromMinutes(1));
+
+	try
+	{
+		while (await timer.WaitForNextTickAsync())
+		{
+			Console.WriteLine("Checking for notifications...");
+
+			try
+			{
+				bool found = await rss.RunAsync();
+
+				if (found)
+				{
+					Console.WriteLine("Found notifications");
+				}
+				else
+				{
+					Console.WriteLine("No notifications found");
+				}
+			}
+			catch
+			{
+				continue;
+			}
+		}
+	}
+	catch { }
+};
 
 await discord.ConnectAsync();
 
 Console.WriteLine("Bot Started");
 
-_ = Console.ReadKey();
+await Task.Delay(-1);
--- Bot/SlashCommands.cs
+++ Bot/SlashCommands.cs
@@ -1,23 +1,16 @@
-﻿using Bot.DataAccess;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.Entities;
+﻿using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.SlashCommands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
 
 namespace Bot
 {
-	public sealed class SlashCommands : ApplicationCommandModule
+	internal sealed class SlashCommands : ApplicationCommandModule
 	{
 		private readonly Channels _channels;
 
-		public SlashCommands()
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>")]
+		public SlashCommands(Channels channels)
 		{
-			_channels = new();
+			_channels = channels;
 		}
 
 		private static async Task<(bool, string)> HandleChannelAsync(InteractionContext context, Func<ChannelModel, Task<bool>> func)
@@ -26,28 +19,27 @@ namespace Bot
 
 			ulong channelId = channel.Id;
 
-			ChannelModel model = new()
-			{
-				Id = channelId
-			};
+			ChannelModel model = new(channelId);
 
 			return (await func(model), string.Join('@', channel.Name, channelId));
 		}
 
 		[SlashCommand("join", "Joins the current channel.")]
+		[RequireRoles(RoleCheckMode.Any, "Owner", "Admin")]
 		public async Task Join(InteractionContext context)
 		{
 			(bool success, string responseText) = await HandleChannelAsync(context, _channels.SaveModelAsync);
 
-			await context.CreateResponseAsync(success ? $"Joined channel {responseText}" : $"Already in channel {responseText}");
+			await context.CreateResponseAsync(success ? $"Joined channel {responseText}" : $"Already in channel {responseText}", true);
 		}
 
 		[SlashCommand("leave", "Leaves the current channel.")]
+		[RequireRoles(RoleCheckMode.Any, "Owner", "Admin")]
 		public async Task Leave(InteractionContext context)
 		{
 			(bool success, string responseText) = await HandleChannelAsync(context, _channels.DeleteModelAsync);
 
-			await context.CreateResponseAsync(success ? $"Left channel {responseText}" : $"Already left channel {responseText}");
+			await context.CreateResponseAsync(success ? $"Left channel {responseText}" : $"Already left channel {responseText}", true);
 		}
 	}
 }
