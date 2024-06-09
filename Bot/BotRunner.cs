8e598e871a40872f14bb2ccf70640f2491123f09
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2024-06-09 01:15:17 -0700

    Copied Converters
    Copied converters from BotSharedLib to the Bot.

---
+++ Bot/BotRunner.cs
@@ -0,0 +1,20 @@
+﻿using Bot.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+	internal sealed class BotRunner
+	{
+		private static readonly JsonSerializerOptions _options = new()
+		{
+			Converters = {
+				new EnumConverter(),
+				new TimeSpanConverter()
+			}
+		};
+	}
+}
---
+++ Bot/Converters/TimeSpanConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace Bot.Converters
+{
+	internal sealed class TimeSpanConverter : JsonConverter<TimeSpan>
+	{
+		public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			return TimeSpan.FromSeconds(reader.GetInt32());
+		}
+
+		public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+		{
+			writer.WriteNumberValue(value.TotalSeconds);
+		}
+	}
+}
